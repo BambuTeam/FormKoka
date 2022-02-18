c8e567b00eb46dab9952695b5158e912337b236e
Oscar_Lopez1720 <oscarantonio.lopez@suizoamericano.edu.gt>
2022-02-18 12:25:57 -0600

    Cambios en vista
    Cambios en vista

--- src/index.js
+++ src/index.js
@@ -19,28 +19,15 @@ class ComponentToPrint extends React.Component {
             <Col>
             <h5 className='txt_tittle'>ESTIMATE <span className='txt_tittle_2'>INFORMATION</span> </h5>
             <div className='shape'></div>
-            <br />
+            
             <input class="input_full" type="text" id="fname" name="fname"></input>
             <Form.Label> Company Name</Form.Label>
             </Col>
             <Col>
             <h5 className='txt_tittle'>BILLING <span className='txt_tittle_2'>INFORMATION</span> </h5>
             <div className='shape'></div>
-            </Col>
-          </Row>
-          
-          <Row class=" mt_fix">
-
-            <Col class="col-2 mt_fix">
-            </Col>
-
-
-            <Col class="col-10">
-            <form>
-              <div class="form-group mt_fix">
-
-
-                <Form.Label>Customer Pickup</Form.Label>
+            
+            <Form.Label>Customer Pickup</Form.Label>
                 <input class="input_billing" type="text" id="fname" name="fname"></input>
 
 
@@ -50,59 +37,34 @@ class ComponentToPrint extends React.Component {
 
                 <Form.Label>Time</Form.Label>
                 <input class="input_billing" type="text" id="fname" name="fname"></input>
-
-              </div>
-            </form>
+                <strong><div>Drop Off</div></strong>
             </Col>
-
-
-
           </Row>
-
-
         </div>
         <div>
           <Row>
             <Col>
             <input class="input_full" type="text" id="fname" name="fname"></input>
             <Form.Label> Client Name</Form.Label>
-            </Col>
-            <Col class="row_break">
-          <strong><div>Drop Off</div></strong>
-            <input class="input_33 gx-5" ></input> 
-            <input  class="input_33 gx-5" ></input> 
-            <input  class="input_33 gx-5"></input> 
-            </Col>
-          </Row>
-        </div>
-
-        <div>
-          <Row>
-            <Col>
-            <input class="input_full" type="text" id="fname" name="fname"></input>
-            <Form.Label> Address</Form.Label>
-            </Col>
-            <Col >
-            <Form.Label class="label_33_l "> Company</Form.Label>
-            <Form.Label class="label_33_c"> Client</Form.Label>
-            <Form.Label class="label_33_r"> Phone #</Form.Label>
-            </Col>
-          </Row>
-        </div>
-
-        <div>
-        <Row>
-            <Col>
             <input class="input_full" type="text" id="fname" name="fname"></input>
             <Form.Label> Phone #</Form.Label>
             </Col>
-            <Col>
-            <Form.Label class=" m-t">Address</Form.Label>
-            <input class="input_full " type="text" id="fname" name="fname"></input>
+            <Col class="row_break">
+          
+            <input class="input_33 gx-5" ></input> 
+            <input  class="input_33 gx-5" ></input> 
+            <input  class="input_33 gx-5"></input> 
+            <Form.Label class="label_33_l "> Company</Form.Label>
+            <Form.Label class="label_33_c"> Client</Form.Label>
+            <Form.Label class="label_33_r"> Phone #</Form.Label>
+            <input class="input_full" type="text" id="fname" name="fname"></input>
+            <Form.Label> Address</Form.Label>
             </Col>
           </Row>
         </div>
 
+        
+       
         <div>
         <Row>
             <Col>
@@ -123,55 +85,37 @@ class ComponentToPrint extends React.Component {
             <Form.Label class="label_33_l "> Phone #</Form.Label>
             
             <Form.Label class="label_33_r2"> Email Address</Form.Label>
+            <h5 className='txt_tittle'>PROJECT <span className='txt_tittle_2'>INFORMATION</span> </h5>
+            <div className='shape'></div>
+            <br />
+            <input class="input_50" type="date" id="fname" name="fname"></input>
+            <input class="input_50" type="date" id="fname" name="fname"></input>
+            <Form.Label class="label_33_l "> Date In</Form.Label>
+            <Form.Label class="label_33_r2"> Due Date</Form.Label>
             </Col>
             <Col>
             <Form.Label class="label_33_l "> Delivery $</Form.Label>
             <Form.Label class="label_33_c"> Gas $</Form.Label>
             <Form.Label class="label_33_r"> Toll $ </Form.Label>
-            
-            </Col>
-          </Row>
-        </div>
-
-        <div>
-          <Row>
-            <Col>
-           
-            </Col>
-            <Col class="row_break">
-            <br />
-          <strong><div>Shipping</div></strong>
+            <strong><div>Shipping</div></strong>
             <input class="input_33 gx-5" ></input> 
             <input  class="input_33 gx-5" ></input> 
             <input  class="input_33 gx-5"></input> 
+            <Form.Label class="label_33_l "> FedEx $</Form.Label>
+            <Form.Label class="label_33_c"> UPS $</Form.Label>
+            <Form.Label class="label_33_r"> Other $ </Form.Label>
+            <strong><div>Third Party Shipping</div></strong>
             </Col>
           </Row>
         </div>
 
-        <Row>
-          <Col>
-          <h5 className='txt_tittle'>PROJECT <span className='txt_tittle_2'>INFORMATION</span> </h5>
-          <div className='shape'></div>
-          <br />
-          <input class="input_50" type="date" id="fname" name="fname"></input>
-          <input class="input_50" type="date" id="fname" name="fname"></input>
-          </Col>
-          <Col>
-          
-          <Form.Label class="label_33_l "> FedEx $</Form.Label>
-            <Form.Label class="label_33_c"> UPS $</Form.Label>
-            <Form.Label class="label_33_r"> Other $ </Form.Label>
-            <br />
-          <strong><div>Third Party Shipping</div></strong>
-          </Col>
-          </Row>
-
           <div>
-        <Row>
+          <Row>
             <Col>
-            <Form.Label class="label_33_l "> Date In</Form.Label>
-            
-            <Form.Label class="label_33_r2"> Due Date</Form.Label>
+            <input class="input_full" type="text" id="fname" name="fname"></input>
+            <Form.Label> Project Name</Form.Label>
+            <input class="input_full" type="text" id="fname" name="fname"></input>
+            <Form.Label> Job Description</Form.Label>
             </Col>
             <Col>
             <input class="input_50_r" type="text" id="fname" name="fname"></input>
@@ -187,18 +131,8 @@ class ComponentToPrint extends React.Component {
         <div>
         <Row>
             <Col>
-            <input class="input_full" type="text" id="fname" name="fname"></input>
-            <Form.Label> Project Name</Form.Label>
-            </Col>
-            <Col></Col>
-          </Row>
-        </div>
-
-        <div>
-        <Row>
-            <Col>
-            <input class="input_full" type="text" id="fname" name="fname"></input>
-            <Form.Label> Job Description</Form.Label>
+            <h5 className='txt_tittle'>SPEC <span className='txt_tittle_2'>INFORMATION</span> </h5>
+            <div className='shape'></div>
             </Col>
             <Col>
             <h5 className='txt_tittle'>BCS COST <span className='txt_tittle_2'>INFORMATION</span> </h5>
@@ -210,8 +144,7 @@ class ComponentToPrint extends React.Component {
         <div>
         <Row>
             <Col>
-            <h5 className='txt_tittle'>SPEC <span className='txt_tittle_2'>INFORMATION</span> </h5>
-          <div className='shape'></div>
+           
           <br />
             </Col>
             <Col>
@@ -262,7 +195,7 @@ class ComponentToPrint extends React.Component {
         <Row>
             <Col>
             <input class="input_billing_2" type="checkbox" id="fname" name="fname"></input>
-            <Form.Label>70# Opaque Text</Form.Label>
+            <Form.Label>70#Opaque Text</Form.Label>
             
             <input class="input_billing_2" type="checkbox" id="fname" name="fname"></input>
             <Form.Label >80# Gloss Text</Form.Label>
@@ -312,11 +245,22 @@ class ComponentToPrint extends React.Component {
             <input class="input_billing_2" type="checkbox" id="fname" name="fname"></input>
             <Form.Label >13 oz. Vinyl </Form.Label>
             &nbsp;&nbsp;&nbsp;&nbsp;
-            <Form.Label>Other</Form.Label>
+            <Form.Label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Other</Form.Label>
             <input class="input_billing_3" type="text" id="fname" name="fname"></input>
+            <br/>
+            <Form.Label >&nbsp;&nbsp; Binding &nbsp;&nbsp;</Form.Label>
+            <select class="input_33" type="select" id="fname" name="fname">
+            <option value="volvo">Saddle Stitch</option>
+            <option value="saab">Perfect Bound</option>
+            <option value="fiat">Wire-O</option>
+            <option value="audi">Ring Binders</option>  
+            </select> 
+            <Form.Label >&nbsp; &nbsp;Production Time  &nbsp; &nbsp;   </Form.Label>
+            <input class="input_33" type="text" id="fname" name="fname"></input>
             </Col>
             
             <Col>
+            <br/>
             <Form.Label class="label_33_l "> Production Time</Form.Label>
             <input class="input_50_r" type="text" id="fname" name="fname"></input>
             
@@ -329,15 +273,7 @@ class ComponentToPrint extends React.Component {
         <div>
         <Row>
             <Col>
-            <Form.Label class="label_33_l "> Binding</Form.Label>
-            <select class="input_33" type="select" id="fname" name="fname">
-            <option value="volvo">Saddle Stitch</option>
-            <option value="saab">Perfect Bound</option>
-            <option value="fiat">Wire-O</option>
-            <option value="audi">Ring Binders</option>  
-            </select> <pre />
-            <Form.Label class="label_33_l "> Production Time</Form.Label>
-            <input class="input_33" type="text" id="fname" name="fname"></input>
+           
             </Col>
             
             <Col>
